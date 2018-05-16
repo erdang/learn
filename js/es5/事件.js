258f7e9b43c2cdd212a174da059fd4311d6da3db
erdang <924064382@qq.com>
2018-05-16 16:52:45 +0800

    33333

--- js/es5/事件.js
+++ js/es5/事件.js
@@ -1,5 +1,5 @@
 事件流描述的是从页面中接收事件的顺序
-
+IE 的事件流叫做事件冒泡
 所有现代浏览器都支持事件冒泡
 
 DOM2.0 模型将事件处理流程分为三个阶段，即事件捕获阶段、事件处理阶段（冒泡阶段的一部分）、事件冒泡阶段
@@ -73,7 +73,63 @@ window.event.cancelBubble = true 取消冒泡  ie只支持冒泡
 window.event.returnValue = false; 取消默认行为
 
 
-return false 需要取消默认行为和冒泡的同时
+return false 需要取消默认行为和冒泡的同时   不要用return false;来阻止event的默认行为
 •event.preventDefault();
 •event.stopPropagation();
 •停止回调函数执行并立即返回。
+
+clientX 可视区
+pageX  页面
+
+在页面没有滚动的情况下，pageX 和 pageY 的值与 clientX 和 clientY 的值相等
+
+DOMContentLoaded load之前触发 dom树形成
+
+鼠标滚轮事件
+
+事件委托
+oUl.onclick = function(ev){
+  　　　　var ev = ev || window.event;
+  　　　　var target = ev.target || ev.srcElement;
+  　　　　if(target.nodeName.toLowerCase() == 'li'){
+  　 　　　　　　 alert(123);
+  　　　　　　　  alert(target.innerHTML);
+  　　　　}
+  　　}
+
+
+  拖拽
+  1.如果拖拽太快鼠标就会脱离拖拽目标
+	利用div事件冒泡,把move事件和up加在document上
+2.选中文字拖拽,会有问题,(所有浏览器都有)
+	原因:我们的拖拽大代码和浏览器默认的行为发生了冲突，从而影响到了我们的拖拽效果
+	标准下:阻止默认行为
+	非标准IE:利用全局捕获的特性
+3.当我们拖拽的是图片的时候也会有问题
+	原因和解决办法同上(第2点)
+  function drag(obj){
+    obj.onmousedown=function(ev){
+      var ev = ev || event
+      var disX = ev.clientX - this.offsetLeft;
+      var dixY = ev.clientY - this.offsetTop;
+
+      if(obj.setCapture){
+        obj.setCapture()
+      }
+      document.onmousemove = function(ev){
+        var ev = ev || event;
+        obj.style.left = ev.clientX - disX + 'px';
+        obj.style.top = ev.clientY - disY + 'px';
+
+      }
+      document.onmouseup = function(){
+
+        document.onmousemove = null;
+        if (obj.releaseCapture) {
+					obj.releaseCapture();
+				}
+      }
+      ev.preventDefault();
+      ev.returnValue = false;
+    }
+  }
